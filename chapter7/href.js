0fe31a1cb83953052e0ecc6157b9b564e39fe37a
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-05-29 00:07:33 +0900

    chapter7を途中まで

---
+++ chapter7/href.js
@@ -0,0 +1,6 @@
+// プルダウンを選択したら遷移する
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('isbn').addEventListener('change', function() {
+        location.href = 'http://www.wings.msn.to/index.php/-A-03/' + this.value;
+    }, false);
+}, false);
